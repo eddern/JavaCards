ec461f5d4dac65ee66b1631aa68b842eaba74e50
eddern <edvardsj@stud.ntnu.no>
2016-04-19 18:18:41 +0200

    added some logic to the game

--- src/Twisted8/Game.java
+++ src/Twisted8/Game.java
@@ -5,6 +5,7 @@ import Deck.Deck;
 import Deck.Card;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * Created by Eirik Kaldahl on 19.04.2016.
@@ -17,30 +18,42 @@ public class Game {
     private ArrayList<Card> board;
 
     public Game(int players){
+        if(10 > 52-(players*5)){
+            throw new IllegalArgumentException("Too many Players.");
+        }
         this.gameIsStarted = false;
         this.deck = new Deck();
         this.players = getPlayers(players);
         this.board = new ArrayList<>();
-
         placeCard(deck.getTopOfDeck());
-        
     }
 
     public ArrayList<Player> getPlayers(int players){
-
         ArrayList<Player> ppl = new ArrayList<>();
         for(int i = 0; i < players; i++){
             ppl.add(new Player("Player "+String.valueOf(i+1)));
         }
-
         return ppl;
     }
 
+
     public void placeCard(Card card){
-        // TODO: make isValid() func
-        board.add(0,card);
+        if(isValid(card)) board.add(0,card);
+        if(card.getValue() == 8) twist();
     }
 
+    public void twist(){
+        Scanner sc = new Scanner(System.in);
+        int twistTo = ;
+        // TODO: make this
+    }
+
+    public boolean isValid(Card card){
+        if(card.getSuit() == getTopOfBoard().getSuit()) return true;
+        if(card.getValue() == getTopOfBoard().getValue()) return true;
+        if(card.getValue() == 8) return true;
+        return false;
+    }
     public Card getTopOfBoard(){
         return board.get(0);
     }
@@ -60,11 +73,6 @@ public class Game {
     }
     public void shufflePlayers(){
         Collections.shuffle(this.players);
-    }
-
-
-    public void run(){
-
     }
 
     public static void main(String[] args) {
